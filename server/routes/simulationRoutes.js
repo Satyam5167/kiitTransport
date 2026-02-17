a502fe7edbbee0e423bf74b983312138fcfbd892
Satyam5167 <satyamgdbg@gmail.com>
2026-02-17 21:14:47 +0530

    removed some services

--- server/routes/simulationRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { updateBusLocation } from '../controllers/simulationController.js';
-
-const router = express.Router();
-
-router.post('/location', updateBusLocation);
-
-export default router;
